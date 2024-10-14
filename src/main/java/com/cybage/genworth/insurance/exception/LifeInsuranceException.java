da8d172a53736a1d9432aa3444456a992a24acdd
Sanket Goykar <sanketgoykar2001@gmail.com>
2024-10-15 00:29:02 +0530

    first commit

---
+++ src/main/java/com/cybage/genworth/insurance/exception/LifeInsuranceException.java
@@ -0,0 +1,24 @@
+package com.cybage.genworth.insurance.exception;
+/**
+*
+* @author Jeevan
+*/  
+public class LifeInsuranceException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	private String errorMessage;
+
+	public LifeInsuranceException(String errorMessage) {
+		super(); //calling constructor Parent RunTime
+		this.errorMessage = errorMessage;
+	}
+	
+	public String getErrorMessage() {
+		return errorMessage;
+	}
+	public void setErrorMessage(String errorMessage) {
+		this.errorMessage = errorMessage;
+	}
+	
+}
